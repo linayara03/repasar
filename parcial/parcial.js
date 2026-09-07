75718da83a553a4d63fa062830f76e247648240f
linayara03 <u20251235535@usco.edu.co>
2026-09-07 08:13:08 -0500

    parcial

---
+++ parcial/parcial.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click", () => {
+    function mostrarMensaje() {
+        mensaje("This is a longer card with supporting text below as a natural lead-in to additional "
+              + "content. This content is a little bit longer.");
+    }
+    mostrarMensaje();
+});
+
+
+
+function credenciales() {
+    const credenciales = prompt("Por favor, ingrese sus credenciales:"); 
+}
